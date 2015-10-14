6bb1ebd87045076565cc6ea8cea33a57e918150f
foxdonut <fdaoud@fastmail.com>
2015-10-14 17:24:04 -0400

    Basic route to return todoList.

--- server/app.js
+++ server/app.js
@@ -3,8 +3,32 @@ var koa = require("koa");
 
 var app = koa();
 
-app.use(function*() {
-  this.body = "Hello Koa";
-});
+var routes = require("koa-route");
+var parse = require("co-body");
+
+var createTodoList = function() {
+  return [
+    {id: 1, priority: 1, description: "Buy more beer"},
+    {id: 2, priority: 1, description: "Order pizza"},
+    {id: 3, priority: 2, description: "Eat pie"},
+    {id: 4, priority: 4, description: "Watch TV"},
+    {id: 5, priority: 5, description: "Sleep"}
+  ];
+}
+var todoList = [];
+
+var getTodoList = function() {
+  if (todoList.length === 0) {
+    todoList = createTodoList();
+  }
+  return todoList;
+};
+
+
+var sendTodoList = function*() {
+  this.body = getTodoList();
+};
+
+app.use(routes.get("/todoList", sendTodoList));
 
 module.exports = app;
--- server/main.js
+++ server/main.js
@@ -4,3 +4,5 @@ var app = require("./app");
 var port = process.env.PORT || 3000;
 
 app.listen(port);
+
+console.log("Server is listening on port", port);
