f001cc7aa355b2fbe0d6db53fbc859fe5203c90d
foxdonut <fdaoud@fastmail.com>
2015-10-13 21:13:19 -0400

    iteration 02

--- client/todoList.jsx
+++ client/todoList.jsx
@@ -1,32 +1,37 @@
-/** @jsx Cycle.hJSX */
+/** @jsx CycleDOM.hJSX */
+import Cycle from "@cycle/core";
+import CycleDOM from "@cycle/dom";
+
 let todoList = function(todos$) {
   let renderTodo = function(todo) {
     return (
       <tr key={todo.id}>
         <td>{todo.priority}</td>
         <td>{todo.description}</td>
-        <td><button className="btn btn-danger btn-xs" data-action="delete" onClick={onDelete(todo)}>Delete</button></td>
+        <td><button className="btn btn-danger btn-xs">Delete</button></td>
       </tr>
     );
   };
 
-  let view$ = todos$.map((todos) => {
-    <div>
-      <div>Todo List:</div>
-      <table className="table ng-table">
-        <thead>
-          <tr>
-            <th>Priority</th>
-            <th>Description</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map(renderTodo)}
-        </tbody>
-      </table>
-    </div>
-  });
+  let view$ = todos$.map(function(todos) {
+    return (
+      <div>
+        <div>Todo List:</div>
+        <table>
+          <thead>
+            <tr>
+              <th>Priority</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map(renderTodo)}
+          </tbody>
+        </table>
+      </div>
+    );
+  })
 
   return {
     DOM: view$
