4ef56622e8d40a106f60453b73240b9ec2befd3b
foxdonut <fdaoud@fastmail.com>
2015-10-14 13:05:00 +0000

    Hello Koa

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
 // app.js
+var koa = require("koa");
+
+var app = koa();
+
+app.use(function*() {
+  this.body = "Hello Koa";
+});
+
+module.exports = app;
--- server/main.js
+++ server/main.js
@@ -1,1 +1,6 @@
 // main.js
+var app = require("./app");
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
