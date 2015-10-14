20f4fd69ba71cd9ad18368734faf4c9c988ae336
foxdonut <fdaoud@fastmail.com>
2015-10-14 12:42:05 +0000

    Added Ramda

--- client/main.js
+++ client/main.js
@@ -1,10 +1,9 @@
 // main.js
+import R from "ramda";
 import events from "./events";
 import model from "./model";
 import todoList from "./todoList.jsx";
 
-let main = (responses) => {
-  return todoList(model(events(responses.DOM)));
-};
+let main = R.pipe(R.prop("DOM"), events, model, todoList);
 
 export default main;
